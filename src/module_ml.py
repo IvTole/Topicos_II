d353c9fa0441eba050b32f26df6ada8374aaf95c
IvTole <toledano.ivan16@gmail.com>
2025-09-23 21:09:01 -0600

    update module_ml.py

--- src/module_ml.py
+++ src/module_ml.py
@@ -11,6 +11,25 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, roc_auc_score
 
+# mlflow
+import mlflow
+from mlflow.models.signature import infer_signature
+
+def mlflow_logger(func):
+    def wrapper(*args, **kwargs):
+        # creamos un nuevo experimento
+        mlruns_path = "../mlruns"
+        mlflow.set_tracking_uri(mlruns_path)
+        experiment_name = "WIDS2024"
+
+        try:
+            exp_id = mlflow.create_experiment(name=experiment_name)
+        except Exception as e:
+            exp_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
+        with mlflow.start_run(experiment_id=exp_id):
+            return func(*args, **kwargs)
+    return wrapper
+
 
 class Model():
 
@@ -27,7 +46,9 @@ class Model():
                                                             )
         return X_train, X_test, y_train, y_test
 
+    @mlflow_logger # decorator
     def evaluate(self, model):
+        print(f"Método: {type(model).__name__}")
         X_train, X_test, y_train, y_test = self.split()
         model.fit(X_train, y_train)
         print("Entrenamiento completado")
@@ -37,3 +58,9 @@ class Model():
         roc_score = roc_auc_score(y_test, y_pred)
         print(f"Accuracy: {accuracy}")
         print(f"Roc_Score: {roc_score}")
+
+        # Registros con mlflow
+        mlflow.log_param("Model Type", type(model).__name__)
+        mlflow.log_metric("accuracy", accuracy)
+        signature = infer_signature(X_train, model.predict(X_train))
+        mlflow.sklearn.log_model(model, "model", signature=signature)
